f697b6ce395a7bc52738c8ee5d5fa2e413de777b
Tommy Chen <tommy351@gmail.com>
2020-05-03 17:19:20 +0800

    feat(cert-manager): Build legacy CRDs

--- packages/cert-manager/__tests__/class.ts
+++ packages/cert-manager/__tests__/class.ts
@@ -1,10 +1,75 @@
-import { Certificate } from "../gen/cert-manager.io/v1alpha3/Certificate";
+import { Certificate as CertificateV1Alpha1 } from "../gen/certmanager.k8s.io/v1alpha1/Certificate";
+import { Certificate as CertificateV1Alpha3 } from "../gen/cert-manager.io/v1alpha3/Certificate";
 
 describe("Certificate", () => {
-  let cert: Certificate;
+  let cert: CertificateV1Alpha1;
 
   beforeEach(() => {
-    cert = new Certificate({
+    cert = new CertificateV1Alpha1({
+      metadata: {
+        name: "foo"
+      },
+      spec: {
+        secretName: "foo-secret",
+        dnsNames: ["foo.example.com"],
+        acme: {
+          config: [
+            {
+              domains: ["foo.example.com"]
+            }
+          ]
+        },
+        issuerRef: {
+          name: "letsencrypt-prod",
+          kind: "Issuer"
+        }
+      }
+    });
+  });
+
+  it("should set apiVersion", () => {
+    expect(cert).toHaveProperty("apiVersion", "certmanager.k8s.io/v1alpha1");
+  });
+
+  it("should set kind", () => {
+    expect(cert).toHaveProperty("kind", "Certificate");
+  });
+
+  it("validate", () => {
+    expect(() => cert.validate()).not.toThrow();
+  });
+
+  it("toJSON", () => {
+    expect(cert.toJSON()).toEqual({
+      apiVersion: "certmanager.k8s.io/v1alpha1",
+      kind: "Certificate",
+      metadata: {
+        name: "foo"
+      },
+      spec: {
+        secretName: "foo-secret",
+        dnsNames: ["foo.example.com"],
+        acme: {
+          config: [
+            {
+              domains: ["foo.example.com"]
+            }
+          ]
+        },
+        issuerRef: {
+          name: "letsencrypt-prod",
+          kind: "Issuer"
+        }
+      }
+    });
+  });
+});
+
+describe("Certificate v1alpha3", () => {
+  let cert: CertificateV1Alpha3;
+
+  beforeEach(() => {
+    cert = new CertificateV1Alpha3({
       metadata: {
         name: "foo"
       },
