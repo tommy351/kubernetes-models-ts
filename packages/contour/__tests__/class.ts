64d0e9eeda69770a7a212148aec57289e687e8e1
Tommy Chen <tommy351@gmail.com>
2020-10-14 01:26:55 +0800

    feat(contour): Upgrade to 1.9.0

--- packages/contour/__tests__/class.ts
+++ packages/contour/__tests__/class.ts
@@ -1,4 +1,5 @@
 import { IngressRoute } from "../gen/contour.heptio.com/v1beta1/IngressRoute";
+import { HTTPProxy } from "../gen/projectcontour.io/v1/HTTPProxy";
 
 describe("IngressRoute", () => {
   let route: IngressRoute;
@@ -53,6 +54,80 @@ describe("IngressRoute", () => {
         routes: [
           {
             match: "/",
+            services: [
+              {
+                name: "foo",
+                port: 80
+              }
+            ]
+          }
+        ]
+      }
+    });
+  });
+});
+
+describe("HTTPProxy", () => {
+  let proxy: HTTPProxy;
+
+  beforeEach(() => {
+    proxy = new HTTPProxy({
+      metadata: {
+        name: "foo"
+      },
+      spec: {
+        virtualhost: {
+          fqdn: "foo.example.com"
+        },
+        routes: [
+          {
+            conditions: [
+              {
+                prefix: "/"
+              }
+            ],
+            services: [
+              {
+                name: "foo",
+                port: 80
+              }
+            ]
+          }
+        ]
+      }
+    });
+  });
+
+  it("should set apiVersion", () => {
+    expect(proxy).toHaveProperty("apiVersion", "projectcontour.io/v1");
+  });
+
+  it("should set kind", () => {
+    expect(proxy).toHaveProperty("kind", "HTTPProxy");
+  });
+
+  it("validate", () => {
+    expect(() => proxy.validate()).not.toThrow();
+  });
+
+  it("toJSON", () => {
+    expect(proxy.toJSON()).toEqual({
+      apiVersion: "projectcontour.io/v1",
+      kind: "HTTPProxy",
+      metadata: {
+        name: "foo"
+      },
+      spec: {
+        virtualhost: {
+          fqdn: "foo.example.com"
+        },
+        routes: [
+          {
+            conditions: [
+              {
+                prefix: "/"
+              }
+            ],
             services: [
               {
                 name: "foo",
