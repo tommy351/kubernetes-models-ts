c91c84eff724f9bff29c17e91065104424566c1e
Tommy Chen <tommy351@gmail.com>
2021-02-28 23:32:26 +0800

    fix(validate): Fix ajv import in ES modules

--- packages/validate/src/validate.ts
+++ packages/validate/src/validate.ts
@@ -1,9 +1,9 @@
-import { ValidationError } from "ajv";
+import Ajv from "ajv";
 import { ajv } from "./ajv";
 
 export function validate(id: string, data: unknown): void {
   if (!ajv.validate(id, data) && ajv.errors) {
-    const err = new ValidationError(ajv.errors);
+    const err = new Ajv.ValidationError(ajv.errors);
     err.message = ajv.errorsText(ajv.errors);
     throw err;
   }
