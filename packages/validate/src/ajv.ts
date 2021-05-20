825f40724bb606a7b151edcb9c512002f8026ca3
Tommy Chen <tommy351@gmail.com>
2021-05-21 00:35:12 +0800

    fix(validate): Fix byte format validator
    The byte format validator in ajv-formats always returns false after running multiple times.
    Use a customized byte format validator as workaround.

--- packages/validate/src/ajv.ts
+++ packages/validate/src/ajv.ts
@@ -1,6 +1,10 @@
 import Ajv from "ajv";
 import addFormats from "ajv-formats";
 
+// From: https://github.com/miguelmota/is-base64/blob/0702e189090921a2f11b4342f27906ff8c43d7ec/is-base64.js#L15
+const rBase64 =
+  /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/;
+
 export const ajv = new Ajv({
   strictTypes: false
 });
@@ -13,3 +17,8 @@ export function register(id: string, schema: object): void {
     ajv.addSchema(schema, id);
   }
 }
+
+ajv.addFormat("byte", {
+  type: "string",
+  validate: (value: string) => rBase64.test(value)
+});
