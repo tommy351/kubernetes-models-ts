f65849fe3e774b9ac3df78c10c7bae9c6b01c0e9
Tommy Chen <tommy351@gmail.com>
2021-08-06 18:35:06 +0800

    chore(publish-scripts): Remove unused file

--- internal/publish-scripts/src/utils.ts
+++
@@ -1,7 +0,0 @@
-import { pathExists, copy } from "fs-extra";
-
-export async function copyIfExists(src: string, dest: string): Promise<void> {
-  if (await pathExists(src)) {
-    await copy(src, dest);
-  }
-}
