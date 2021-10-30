9575aff47df67aef6633bb2ad73f3b1099501ac0
Tommy Chen <tommy351@gmail.com>
2021-10-30 10:03:05 +0800

    test(kapp-controller): Add tests and update readme

--- packages/kapp-controller/README.md
+++ packages/kapp-controller/README.md
@@ -10,6 +10,34 @@ Install with npm.
 npm install @kubernetes-models/kapp-controller
 ```
 
+## Usage
+
+```js
+import { App } from "@kubernetes-models/kapp-controller/kappctrl.k14s.io/v1alpha1/App";
+
+// Create a new Application
+const app = new App({
+  metadata: { name: "hello" },
+  spec: {
+    serviceAccountName: "default-ns-sa",
+    fetch: [
+      {
+        git: {
+          url: "https://github.com/vmware-tanzu/carvel-simple-app-on-kubernetes",
+          ref: "origin/develop",
+          subPath: "config-step-2-template"
+        }
+      }
+    ],
+    template: [{ ytt: {} }],
+    deploy: [{ kapp: {} }]
+  }
+});
+
+// Validate against JSON schema
+app.validate();
+```
+
 ## License
 
 See [LICENSE](../../LICENSE)
---
+++ packages/kapp-controller/__tests__/class.ts
@@ -0,0 +1,59 @@
+import { App } from "../gen/kappctrl.k14s.io/v1alpha1/App";
+
+describe("App", () => {
+  let app: App;
+
+  beforeEach(() => {
+    app = new App({
+      metadata: { name: "hello" },
+      spec: {
+        serviceAccountName: "default-ns-sa",
+        fetch: [
+          {
+            git: {
+              url: "https://github.com/vmware-tanzu/carvel-simple-app-on-kubernetes",
+              ref: "origin/develop",
+              subPath: "config-step-2-template"
+            }
+          }
+        ],
+        template: [{ ytt: {} }],
+        deploy: [{ kapp: {} }]
+      }
+    });
+  });
+
+  it("should set apiVersion", () => {
+    expect(app).toHaveProperty("apiVersion", "kappctrl.k14s.io/v1alpha1");
+  });
+
+  it("should set kind", () => {
+    expect(app).toHaveProperty("kind", "App");
+  });
+
+  it("validate", () => {
+    expect(() => app.validate()).not.toThrow();
+  });
+
+  it("toJSON", () => {
+    expect(app.toJSON()).toEqual({
+      apiVersion: "kappctrl.k14s.io/v1alpha1",
+      kind: "App",
+      metadata: { name: "hello" },
+      spec: {
+        serviceAccountName: "default-ns-sa",
+        fetch: [
+          {
+            git: {
+              url: "https://github.com/vmware-tanzu/carvel-simple-app-on-kubernetes",
+              ref: "origin/develop",
+              subPath: "config-step-2-template"
+            }
+          }
+        ],
+        template: [{ ytt: {} }],
+        deploy: [{ kapp: {} }]
+      }
+    });
+  });
+});
