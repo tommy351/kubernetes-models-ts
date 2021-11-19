cdd87e2fba43d2051be874f3c59a33b1919ffa38
Rueian <rueiancsie@gmail.com>
2021-11-20 01:45:39 +0800

    feat(cilium): Add package

---
+++ packages/cilium/__tests__/validate.ts
@@ -0,0 +1,57 @@
+import { CiliumLocalRedirectPolicy } from "../gen/cilium.io/v2/CiliumLocalRedirectPolicy";
+
+describe("validate", () => {
+  describe("when validation passed", () => {
+    it("should pass", () => {
+      const lrp = new CiliumLocalRedirectPolicy({
+        metadata: {
+          name: "lrp"
+        },
+        spec: {
+          redirectFrontend: {
+            serviceMatcher: {
+              serviceName: "my-service",
+              namespace: "default"
+            }
+          },
+          redirectBackend: {
+            localEndpointSelector: {
+              matchLabels: {
+                name: "proxy"
+              }
+            },
+            toPorts: [
+              {
+                port: "8080",
+                protocol: "TCP"
+              }
+            ]
+          }
+        }
+      });
+
+      expect(() => lrp.validate()).not.toThrow();
+    });
+  });
+
+  describe("when validation failed", () => {
+    it("should throw an error", () => {
+      const lrp = new CiliumLocalRedirectPolicy({
+        metadata: {
+          name: "lrp"
+        },
+        spec: {
+          redirectFrontend: {},
+          redirectBackend: {
+            localEndpointSelector: {},
+            toPorts: []
+          }
+        }
+      });
+
+      expect(() => lrp.validate()).toThrow(
+        "data/spec/redirectFrontend must have required property 'addressMatcher', data/spec/redirectFrontend must have required property 'serviceMatcher', data/spec/redirectFrontend must match exactly one schema in oneOf"
+      );
+    });
+  });
+});
