7deed2b73aaa23afb1767f0e88bd15a5e6475c66
Tommy Chen <tommy351@gmail.com>
2024-04-01 23:19:03 +0800

    feat(generate): Compress schema files

--- utils/crd-generate/src/generators/schema.ts
+++ utils/crd-generate/src/generators/schema.ts
@@ -6,17 +6,19 @@ import {
 import { getSchemaPath } from "../utils";
 
 const generateSchemas: Generator = async (definitions) => {
-  return definitions.map((def) => {
-    const schema = transformSchema(def.schema);
+  return Promise.all(
+    definitions.map(async (def) => {
+      const schema = transformSchema(def.schema);
 
-    return {
-      path: getSchemaPath(def.schemaId),
-      content: compileSchema(schema, {
-        "io.k8s.apimachinery.pkg.apis.meta.v1.ObjectMeta":
-          "@kubernetes-models/apimachinery/_schemas/IoK8sApimachineryPkgApisMetaV1ObjectMeta"
-      })
-    };
-  });
+      return {
+        path: getSchemaPath(def.schemaId),
+        content: await compileSchema(schema, {
+          "io.k8s.apimachinery.pkg.apis.meta.v1.ObjectMeta":
+            "@kubernetes-models/apimachinery/_schemas/IoK8sApimachineryPkgApisMetaV1ObjectMeta"
+        })
+      };
+    })
+  );
 };
 
 export default generateSchemas;
