20e66b303049f80908f6edff953f608399cd132b
Yz C <yanzhen610@gmail.com>
2024-11-25 14:43:02 +0800

    chore(third-party/victoria-metrics-operator): bump to version `0.49.1`

---
+++ .changeset/shy-cameras-refuse.md
@@ -0,0 +1,5 @@
+---
+"@kubernetes-models/victoria-metrics-operator": minor
+---
+
+Upgrade to version `0.49.1`
--- third-party/victoria-metrics-operator/scripts/download-crd.ts
+++ third-party/victoria-metrics-operator/scripts/download-crd.ts
@@ -1,28 +1,23 @@
 /* eslint-disable node/no-unpublished-import */
-import JSZip from "jszip";
 import { createWriteStream } from "fs";
 import { mkdir } from "fs/promises";
 import { dirname, join } from "path";
 import streamToPromise from "stream-to-promise";
-import assert from "assert";
 import fetch from "make-fetch-happen";
 import findCacheDir from "find-cache-dir";
 
-const VERSION = "0.37.1";
+const VERSION = "0.49.1";
 
 const outputPath = join(__dirname, "../crds/crd.yaml");
 
 (async () => {
   const res = await fetch(
-    `https://github.com/VictoriaMetrics/operator/releases/download/v${VERSION}/bundle_crd.zip`,
+    `https://github.com/VictoriaMetrics/operator/releases/download/v${VERSION}/crd.yaml`,
     { cachePath: findCacheDir({ name: "victoria-metrics-operator" }) }
   );
-  const zip = await JSZip.loadAsync(await res.buffer());
-  const file = zip.file("release/crds/crd.yaml");
-  assert(file);
 
   await mkdir(dirname(outputPath), { recursive: true });
 
   const dest = createWriteStream(outputPath);
-  await streamToPromise(file.nodeStream().pipe(dest));
+  await streamToPromise(res.body.pipe(dest));
 })();
